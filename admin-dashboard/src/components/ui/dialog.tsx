de5ef22322278af4e7e9491120a5b8fa49364da0
Tayeb Ashraf <tayebashraf@users.noreply.github.com>
2026-05-20 10:32:48 +0600

    fix: add asChild prop type to UI triggers to fix build errors

--- admin-dashboard/src/components/ui/dialog.tsx
+++ admin-dashboard/src/components/ui/dialog.tsx
@@ -11,8 +11,8 @@ function Dialog({ ...props }: DialogPrimitive.Root.Props) {
   return <DialogPrimitive.Root data-slot="dialog" {...props} />
 }
 
-function DialogTrigger({ ...props }: DialogPrimitive.Trigger.Props) {
-  return <DialogPrimitive.Trigger data-slot="dialog-trigger" {...props} />
+function DialogTrigger({ ...props }: DialogPrimitive.Trigger.Props & { asChild?: boolean }) {
+  return <DialogPrimitive.Trigger data-slot="dialog-trigger" {...(props as any)} />
 }
 
 function DialogPortal({ ...props }: DialogPrimitive.Portal.Props) {
